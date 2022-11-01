2f90184ea1c71eed8b35bf2ebb5a4831e37d7cb7
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-11-01 16:36:06 -0300

    Aula 01 - First Commit

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "native-base"
+export function SignIn(){
+  return(
+    <Center flex={1} bgColor="black">
+      <Text color="white" fontSize={24}>
+          SignIn
+      </Text>
+    </Center>
+  )
+}
